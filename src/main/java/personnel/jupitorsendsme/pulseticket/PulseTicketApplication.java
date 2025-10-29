1e7cc48c6711645bb373ac31a5d2b123624974a6
jupitorSendsme <jupitorsendsme@gmail.com>
2025-10-29 21:16:49 +0900

    init

---
+++ src/main/java/personnel/jupitorsendsme/pulseticket/PulseTicketApplication.java
@@ -0,0 +1,13 @@
+package personnel.jupitorsendsme.pulseticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personnel/jupitorsendsme/pulseticket/PulseTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package personnel.jupitorsendsme.pulseticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
