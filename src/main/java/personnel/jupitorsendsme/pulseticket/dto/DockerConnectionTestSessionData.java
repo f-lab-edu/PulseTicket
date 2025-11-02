95ebe615654bd70f49529abdb996c556d5937d1e
jupitorSendsme <jupitorsendsme@gmail.com>
2025-11-03 02:05:04 +0900

    Spring Boot, Redis, PostgreSQL 연결 테스트 2

--- src/main/java/personnel/jupitorsendsme/pulseticket/dto/DockerConnectionTestSessionData.java
+++ src/main/java/personnel/jupitorsendsme/pulseticket/dto/DockerConnectionTestSessionData.java
@@ -1,15 +1,20 @@
 package personnel.jupitorsendsme.pulseticket.dto;
 
+import java.io.Serializable;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
  * Docker 연결 테스트 세션 데이터 DTO
  * 세션 상태를 저장하고 조회하기 위한 간단한 데이터 모델
+ * Redis에 저장하기 위해 Serializable 구현 필요
  */
 @Getter
 @NoArgsConstructor
-public class DockerConnectionTestSessionData {
+public class DockerConnectionTestSessionData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     /**
      * 세션에 저장된 값
