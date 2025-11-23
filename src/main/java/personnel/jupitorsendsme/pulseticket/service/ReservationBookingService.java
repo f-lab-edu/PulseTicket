33f27bc40c8d9f984d0cdd664b3e8ce433b7c767
jupitorSendsme <jupitorsendsme@gmail.com>
2025-11-24 03:27:08 +0900

    ReservationBookingService - book 에서, 메소드를 더 나눔 - 테스트에 용이하게끔

--- src/main/java/personnel/jupitorsendsme/pulseticket/service/ReservationBookingService.java
+++ src/main/java/personnel/jupitorsendsme/pulseticket/service/ReservationBookingService.java
@@ -31,14 +31,31 @@ public class ReservationBookingService {
 	 */
 	public ReservationBookingResponse book(ReservationBookingRequest request) {
 
-		User user = userManagementService.findValidUser(request);
-		Seat seat = reservationQueryService.findAvailableSeat(request);
+		User user = loadUser(request);
+		Seat seat = loadSeat(request);
+
+		Reservation created = makeReservation(user, seat);
+
+		return ReservationBookingResponse.success(created);
+	}
+
+	private User loadUser(ReservationBookingRequest request) {
+
+		return userManagementService.findValidUser(request);
+	}
+
+	private Seat loadSeat(ReservationBookingRequest request) {
+
+		return reservationQueryService.findAvailableSeat(request);
+	}
+
+	private Reservation makeReservation(User user, Seat seat) {
 
 		Reservation reserve = Reservation.reserve(user, seat);
 		Reservation created = reservationRepository.save(reserve);
 
 		seat.reserve();
 
-		return ReservationBookingResponse.success(created);
+		return created;
 	}
 }
