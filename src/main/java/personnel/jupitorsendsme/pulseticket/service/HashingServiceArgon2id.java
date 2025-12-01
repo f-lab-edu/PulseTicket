d16b6240bea55c687fad21a21243fcf8ff3097cc
jupitorSendsme <jupitorsendsme@gmail.com>
2025-12-01 16:18:07 +0900

    단어 통일성 ...

--- src/main/java/personnel/jupitorsendsme/pulseticket/service/HashingServiceArgon2id.java
+++ src/main/java/personnel/jupitorsendsme/pulseticket/service/HashingServiceArgon2id.java
@@ -62,11 +62,11 @@ public class HashingServiceArgon2id {
 	/**
 	 * 입력된 원본 패스워드와 해싱된 패스워드가 일치하는지 확인
 	 * @param rawPassword 비교하고자 하는 원본 패스워드, 이전에 해싱된 패스워드가 담긴 객체
-	 * @param hashedPassword 이전에 해싱된 패스워드
+	 * @param passwordHash 이전에 해싱된 패스워드
 	 * @return 일치 여부
 	 */
-	public boolean verify(String rawPassword, String hashedPassword) {
-		return encoder.matches(rawPassword, hashedPassword);
+	public boolean verify(String rawPassword, String passwordHash) {
+		return encoder.matches(rawPassword, passwordHash);
 	}
 
 	public boolean verify(ReservationRequest request, User user) {
